674ca3ef04de45aa0bfc14681ed136c4149c5b2d
greylight2 <ya-vam-ne@mail.ru>
2021-11-03 10:10:47 +0300

    прописаны хедер и футер

---
+++ views/chanks/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Тестовое задание</title>
+  <style>
+    p {
+      font-size: 18pt;
+      margin: 10px;
+    }
+
+    li {
+      padding-bottom: 10px;
+    }
+    button{
+      padding: 8px;
+      font-size: 18pt;
+    }
+  </style>
+</head>
+
+<body>
